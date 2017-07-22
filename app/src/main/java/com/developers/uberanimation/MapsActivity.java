3ed1950750573a8fe17ffc68a818cf0eb36d1c17
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2017-07-23 02:14:02 +0530

    first commit

---
+++ app/src/main/java/com/developers/uberanimation/MapsActivity.java
@@ -0,0 +1,257 @@
+package com.developers.uberanimation;
+
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+    private List<LatLng> polyLineList;
+    private Marker marker;
+    private float v;
+    private double lat, lng;
+    private Handler handler;
+    private LatLng startPosition, endPosition;
+    private int index, next;
+    private LatLng sydney;
+    private Button button;
+    private EditText destinationEditText;
+    private String destination;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        polyLineList = new ArrayList<>();
+        button = (Button) findViewById(R.id.destination_button);
+        destinationEditText = (EditText) findViewById(R.id.edittext_place);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                destination = destinationEditText.getText().toString();
+                destination = destination.replace(" ", "+");
+                Log.d(TAG, destination);
+                mapFragment.getMapAsync(MapsActivity.this);
+            }
+        });
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        final double latitude = 28.671246;
+        double longitude = 77.317654;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setTrafficEnabled(false);
+        mMap.setIndoorEnabled(false);
+        mMap.setBuildingsEnabled(false);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        // Add a marker in Home and move the camera
+        sydney = new LatLng(28.671246, 77.317654);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Home"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+                .target(googleMap.getCameraPosition().target)
+                .zoom(17)
+                .bearing(30)
+                .tilt(45)
+                .build()));
+        marker = mMap.addMarker(new MarkerOptions().position(sydney)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_car)));
+        String requestUrl = null;
+        try {
+            requestUrl = "https://maps.googleapis.com/maps/api/directions/json?" +
+                    "mode=driving&"
+                    + "transit_routing_preference=less_driving&"
+                    + "origin=" + latitude + "," + longitude + "&"
+                    + "destination=" + destination + "&"
+                    + "key=" + getResources().getString(R.string.google_directions_key);
+            Log.d(TAG, requestUrl);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                    requestUrl, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d(TAG, response + "");
+                            try {
+                                JSONArray jsonArray = response.getJSONArray("routes");
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject route = jsonArray.getJSONObject(i);
+                                    JSONObject poly = route.getJSONObject("overview_polyline");
+                                    String polyline = poly.getString("points");
+                                    polyLineList = decodePoly(polyline);
+                                    Log.d(TAG, polyLineList + "");
+                                }
+                                for (LatLng latLng : polyLineList) {
+                                    mMap.addCircle(new CircleOptions()
+                                            .center(latLng)
+                                            .radius(4)
+                                            .visible(true)
+                                            .clickable(false)
+                                            .fillColor(Color.CYAN)
+                                            .strokeColor(Color.BLUE)
+                                            .strokeWidth(4));
+                                }
+                                marker = mMap.addMarker(new MarkerOptions().position(sydney)
+                                        .flat(true)
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_car)));
+                                handler = new Handler();
+                                index = -1;
+                                next = 1;
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (index < polyLineList.size() - 1) {
+                                            index++;
+                                            next = index + 1;
+                                            Log.d(TAG, index + " <-ind " + " next->" + next);
+                                        }
+                                        if (index < polyLineList.size() - 1) {
+                                            startPosition = polyLineList.get(index);
+                                            endPosition = polyLineList.get(next);
+                                        }
+                                        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
+                                        valueAnimator.setDuration(3000);
+                                        valueAnimator.setInterpolator(new LinearInterpolator());
+                                        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                            @Override
+                                            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                                v = valueAnimator.getAnimatedFraction();
+                                                lng = v * endPosition.longitude + (1 - v)
+                                                        * startPosition.longitude;
+                                                lat = v * endPosition.latitude + (1 - v)
+                                                        * startPosition.latitude;
+                                                LatLng newPos = new LatLng(lat, lng);
+                                                marker.setPosition(newPos);
+                                                marker.setAnchor(0.5f, 0.5f);
+                                                marker.setRotation(getBearing(startPosition, newPos));
+                                                mMap.moveCamera(CameraUpdateFactory
+                                                        .newCameraPosition
+                                                                (new CameraPosition.Builder()
+                                                                        .target(newPos)
+                                                                        .zoom(15.5f)
+                                                                        .build()));
+                                                Log.d(TAG, "Coming: " + newPos);
+                                            }
+                                        });
+                                        valueAnimator.start();
+                                        handler.postDelayed(this, 3000);
+                                    }
+                                }, 3000);
+
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d(TAG, error + "");
+                }
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private List<LatLng> decodePoly(String encoded) {
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
+    private float getBearing(LatLng begin, LatLng end) {
+        double lat = Math.abs(begin.latitude - end.latitude);
+        double lng = Math.abs(begin.longitude - end.longitude);
+
+        if (begin.latitude < end.latitude && begin.longitude < end.longitude)
+            return (float) (Math.toDegrees(Math.atan(lng / lat)));
+        else if (begin.latitude >= end.latitude && begin.longitude < end.longitude)
+            return (float) ((90 - Math.toDegrees(Math.atan(lng / lat))) + 90);
+        else if (begin.latitude >= end.latitude && begin.longitude >= end.longitude)
+            return (float) (Math.toDegrees(Math.atan(lng / lat)) + 180);
+        else if (begin.latitude < end.latitude && begin.longitude >= end.longitude)
+            return (float) ((90 - Math.toDegrees(Math.atan(lng / lat))) + 270);
+        return -1;
+    }
+}
