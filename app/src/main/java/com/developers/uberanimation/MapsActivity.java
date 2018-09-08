382c60a3697f13a3ffa572d9cb717ed8ff763e49
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2018-09-08 20:09:12 +0530

    feat: removed unused imports

--- app/src/main/java/com/developers/uberanimation/MapsActivity.java
+++ app/src/main/java/com/developers/uberanimation/MapsActivity.java
@@ -1,6 +1,5 @@
 package com.developers.uberanimation;
 
-import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.os.Handler;
@@ -8,17 +7,9 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.developers.uberanimation.models.Result;
 import com.developers.uberanimation.models.Route;
 import com.google.android.gms.maps.CameraUpdate;
@@ -28,7 +19,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
@@ -36,14 +26,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.SquareCap;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.w3c.dom.ls.LSException;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -144,7 +128,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                             @Override
                             public void onSubscribe(Disposable d) {
-
 
                             }
 
